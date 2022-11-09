7516f940e5e8bc056bc5ee756ea315650fb3bca6
Maiia Kalajiawi <maiiakalagawi@hotmail.com>
2022-11-10 01:49:56 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import FilterableProductTable from "./components/FilterableProductTable";
 import FilterableCategoryTable from "./components/FilterableCategoryTable";
 import NavBar from "./components/Navigation/NavBar";
 
+
 function App() {
   return (
-    <>
-      <NavBar />
-      <FilterableCategoryTable />
-    </>
+    <StrictMode>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/product-table" element={<FilterableProductTable />} />
+          <Route path="/category-table" element={<FilterableCategoryTable />} />
+          <Route path="/" element={<FilterableCategoryTable />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 }
 
--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -12,8 +12,9 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/product-table">Products</Nav.Link>
+            <Nav.Link href="/category-table">Categories</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
