0a1ecdd49e3609063600a5cd2318b300a38295f1
Maiia Kalajiawi <maiiakalagawi@hotmail.com>
2022-11-13 01:43:14 +0200

    Update MiniCard.jsx

--- src/components/Card/MiniCard.jsx
+++ src/components/Card/MiniCard.jsx
@@ -4,11 +4,13 @@ import './MiniCard.css'
 // props, card size, image size, image=true/false, title=true/false, text=true/false
 
 export function MiniCard({ props, total }) {
-    return <ReactCard className="shadow border cardSize">
-        <img className="icon" src={props.imageURL}></img>
-        <h6 className="titleSize text-muted fw-light">{props.name}</h6>
-        <h6 className="textSize text-muted fw-normal">{total} items</h6>
-  </ReactCard>
+  return (
+    <ReactCard className="shadow border cardSize">
+      <img className="icon" src={props.imageURL}></img>
+      <h6 className="titleSize text-muted fw-light">{props.name}</h6>
+      <h6 className="textSize text-muted fw-normal">{total} items</h6>
+    </ReactCard>
+  )
 }
 
 export default MiniCard
