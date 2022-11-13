5c820aaca42b6cb10f328cb7f7f89f645a8f30d7
Maiia Kalajiawi <maiiakalagawi@hotmail.com>
2022-11-13 02:07:54 +0200

    Update List.jsx

--- src/features/list/components/List.jsx
+++ src/features/list/components/List.jsx
@@ -1,11 +1,13 @@
-import {CategoryList} from './CategoryList'
+import { CategoryList } from './CategoryList'
 import { ProductList } from './ProductList'
+import { CartList } from './CartList'
 
 export function List() {
   return (
-    <>
+      <>
+      <CartList />
       <CategoryList />
-      <ProductList/>
+      <ProductList />
     </>
   )
 }
